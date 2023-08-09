457342e5304697e2a5a09773696c7569f2652c88
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-08-09 13:01:39 +0530

    url update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = ({ showCooie, setShowCookie, count, setCount }) => {
             <footer>
                 <div></div>
                 <div>
-                    <a href="//github.com/scorcism" rel="noreferrer" target="_blank">scorcism</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a href="//github.com/DevOpsProjectOps" rel="noreferrer" target="_blank">DevOpsProjectOps</a>
+                    <a href="//github.com/scorcism" rel="noreferrer" target="_blank">scorcism</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a href="https://github.com/asPerReqirements" rel="noreferrer" target="_blank">asPerReqirements</a>
                 </div>
                 <div>
                     {
                         showCooie &&
-                        <img onClick={getPerk} id="cookie" src={`/cookie.svg`} alt="cookie" />
+                        <img onClick={getPerk} id="cookie" src={`./cookie.svg`} alt="cookie" />
                     }
                 </div>
             </footer>
